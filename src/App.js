b362e38e61e6b597f4b5f34eda7ddcb2488cd311
Mr.Vyas <divyeshvyas562@gmail.com>
2021-10-05 20:54:31 +0530

    name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React By Divyesh vyas
         </a>
       </header>
     </div>
